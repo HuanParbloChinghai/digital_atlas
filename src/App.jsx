ab15b5e98c33d3c8eaa25f419ca74ff8e829b4a7
chingy <chingylyne@gmail.com>
2025-03-17 14:29:20 +0000

    boilplate css code inside the index.css file is cleared and the @ directive for the custom font Nunito added at the top

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1 className='bg-sky-500'>Vite + React</h1>
+      <div className="card bg-sky-200">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
