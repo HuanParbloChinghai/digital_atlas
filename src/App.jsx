159c2a0f5cd08d03aefd474a1e12a260a674136b
chingy <chingylyne@gmail.com>
2025-03-21 18:24:16 +0000

    logic for addition of a dark class to the root element of our DOM to control theming implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [darkMode, setDarkMode] = useState(false);
+
+
+  useEffect(
+    () => {
+      if (darkMode) {
+        document.documentElement.classList.add('dark');
+        console.log('dark mode active');
+      } else {
+        document.documentElement.classList.remove('dark');
+        console.log('light mode active');
+      }
+    }, [darkMode]
+  )
 
   return (
     <>
       <div>
-        
+        {/* <Header /> */}
       </div>
         
     </>
